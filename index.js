d3340cf2b3ab109485de9bf7de00a406c74ed806
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-31 14:41:51 +0100

    Installed dotenv and created .env file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
-const server = require('./server.js');
+require("dotenv").config();
 
-server.listen(8000, () => {
-  console.log('\n* Server Running on http://localhost:8000 *\n');
+const server = require("./server.js");
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
